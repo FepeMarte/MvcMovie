fa5952593bb7d3c91131494394d51ef66d8f72c6
Fepe <felipempduarte@outlook.com>
2019-11-25 12:29:25 -0300

    Projeto iniciado

---
+++ Dados/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Dados
+{
+    public class MvcMovieContext: DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> op): base(op)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Titulo")]
+        public string Title { get; set; }
+        [Display(Name = "Data de Lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Genêro")]
+        public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+
+        [Display(Name = "Preço")]
+        public decimal Price { get; set; }
+    }
+}
